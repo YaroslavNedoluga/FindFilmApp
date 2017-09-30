1c4ffaba371fd80d65e847c1343edc8d7e18f2af
Egor Salenko <eg.salenko@gmail.com>
2017-09-30 11:10:17 +0300

    Update ApiHelper avoid static fields

--- app/src/main/java/io/esalenko/findfilmapp/hepler/ApiHelper.kt
+++ app/src/main/java/io/esalenko/findfilmapp/hepler/ApiHelper.kt
@@ -3,19 +3,18 @@ package io.esalenko.findfilmapp.hepler
 import android.content.Context
 import android.os.Build
 import io.esalenko.findfilmapp.R
+import javax.inject.Inject
 
 
-class ApiHelper {
+class ApiHelper @Inject constructor(val context: Context) {
 
-    companion object {
-        val region = "ISO 3166-1"
-        fun getApiKey(context: Context) = context.getString(R.string.api_key)
+    val region = "ISO 3166-1"
+    fun getApiKey() = context.getString(R.string.api_key)
 
-        fun getLocale(context: Context) = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
-            context.resources.configuration.locale.toString()
-        else
-            context.resources.configuration.locales.get(0).toString()
+    fun getLocale() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
+        context.resources.configuration.locale.toString()
+    else
+        context.resources.configuration.locales.get(0).toString()
 
-    }
 
 }
