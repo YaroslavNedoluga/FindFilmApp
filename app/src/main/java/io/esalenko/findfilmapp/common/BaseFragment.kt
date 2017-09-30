fca4e4f95297d4175aea5577a907f51f48f1f70d
Egor Salenko <eg.salenko@gmail.com>
2017-09-30 10:52:23 +0300

    Add ButterKnife unbinder

--- app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
@@ -1,21 +1,36 @@
 package io.esalenko.findfilmapp.common
 
+import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import butterknife.ButterKnife
+import butterknife.Unbinder
 import dagger.android.support.AndroidSupportInjection
 
 abstract class BaseFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    lateinit var unbinder: Unbinder
+
+    override fun onAttach(context: Context?) {
         AndroidSupportInjection.inject(this)
-        return inflater?.inflate(getLayoutRes(), container, false)
+        super.onAttach(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater?.inflate(getLayoutRes(), container, false)!!
+        unbinder = ButterKnife.bind(this, view)
+        return view
     }
 
     @LayoutRes
     protected abstract fun getLayoutRes(): Int
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        unbinder.unbind()
+    }
 }
