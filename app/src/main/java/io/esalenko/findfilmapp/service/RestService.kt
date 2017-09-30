9e5aef3ebcb8156fb024b0c2e4d9050515e55255
Egor Salenko <eg.salenko@gmail.com>
2017-09-30 11:16:40 +0300

    Refactoring Popular Film

--- app/src/main/java/io/esalenko/findfilmapp/service/RestService.kt
+++ app/src/main/java/io/esalenko/findfilmapp/service/RestService.kt
@@ -1,6 +1,6 @@
 package io.esalenko.findfilmapp.service
 
-import io.esalenko.findfilmapp.model.PopularFilms
+import io.esalenko.findfilmapp.model.PopularFilm
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,6 +12,6 @@ interface RestService {
     fun getPopularFilms(@Query("api_key") key : String,
                         @Query("language") language : String,
                         @Query("page") page : Int,
-                        @Query("region") region : String) : Observable<PopularFilms>
+                        @Query("region") region: String): Observable<PopularFilm>
 
 }
