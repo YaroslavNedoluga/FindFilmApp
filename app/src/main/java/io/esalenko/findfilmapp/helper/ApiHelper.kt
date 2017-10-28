4547cc79c0c98179bb8d3ae440059bc3e4873734
Egor Salenko <wrongua@gmail.com>
2017-10-29 01:22:55 +0300

    Change ApiHelper (remove Dagger)

--- app/src/main/java/io/esalenko/findfilmapp/helper/ApiHelper.kt
+++ app/src/main/java/io/esalenko/findfilmapp/helper/ApiHelper.kt
@@ -1,15 +1,14 @@
-package io.esalenko.findfilmapp.hepler
+package io.esalenko.findfilmapp.helper
 
 import android.content.Context
 import android.os.Build
 import io.esalenko.findfilmapp.R
-import javax.inject.Inject
 
 
-class ApiHelper @Inject constructor(val context: Context) {
+class ApiHelper (val context: Context) {
 
     val region = "ISO 3166-1"
-    fun getApiKey() = context.getString(R.string.api_key)
+    fun getApiKey() = context.getString(R.string.api_key)!!
 
     fun getLocale() = if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N)
         context.resources.configuration.locale.toString()
