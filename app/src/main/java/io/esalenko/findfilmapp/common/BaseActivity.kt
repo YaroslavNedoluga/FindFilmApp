00e13d238f8b2b19943b78217048c0a81e1d48a0
Egor Salenko <wrongua@gmail.com>
2017-10-29 01:20:07 +0300

    Remove dagger

--- app/src/main/java/io/esalenko/findfilmapp/common/BaseActivity.kt
+++ app/src/main/java/io/esalenko/findfilmapp/common/BaseActivity.kt
@@ -3,33 +3,21 @@ package io.esalenko.findfilmapp.common
 import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
-import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
-import dagger.android.AndroidInjection
-import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
-import javax.inject.Inject
 
 
-abstract class BaseActivity : AppCompatActivity(), HasSupportFragmentInjector {
-
-    @Inject
-    lateinit var fragmentInjector : DispatchingAndroidInjector<Fragment>
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
+abstract class BaseActivity :        AppCompatActivity(){
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(getLayoutRes())
     }
 
     @LayoutRes
-    protected abstract fun getLayoutRes() : Int
+    protected abstract fun getLayoutRes(): Int
 }
--- app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
@@ -1,6 +1,5 @@
 package io.esalenko.findfilmapp.common
 
-import android.content.Context
 import android.os.Bundle
 import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
@@ -9,16 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import butterknife.ButterKnife
 import butterknife.Unbinder
-import dagger.android.support.AndroidSupportInjection
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var unbinder: Unbinder
-
-    override fun onAttach(context: Context?) {
-        AndroidSupportInjection.inject(this)
-        super.onAttach(context)
-    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view: View = inflater?.inflate(getLayoutRes(), container, false)!!
