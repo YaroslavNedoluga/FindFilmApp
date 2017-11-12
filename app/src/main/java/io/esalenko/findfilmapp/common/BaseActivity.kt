92bbc4f095ef49681990e25fbbb27f5970c9eb95
Egor Salenko <wrongua@gmail.com>
2017-11-12 21:42:12 +0200

    Refactoring of BaseActivity

--- app/src/main/java/io/esalenko/findfilmapp/common/BaseActivity.kt
+++ app/src/main/java/io/esalenko/findfilmapp/common/BaseActivity.kt
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
 
 
-abstract class BaseActivity :        AppCompatActivity(){
+abstract class BaseActivity : AppCompatActivity() {
 
     override fun attachBaseContext(newBase: Context?) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase))
