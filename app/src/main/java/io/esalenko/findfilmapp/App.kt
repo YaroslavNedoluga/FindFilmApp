4369c06921bb547eb1f6f68bb99beb146a3b1a0e
Egor Salenko <wrongua@gmail.com>
2017-11-12 21:41:29 +0200

    Add Leak Canary and func for get system locale

--- app/src/main/java/io/esalenko/findfilmapp/App.kt
+++ app/src/main/java/io/esalenko/findfilmapp/App.kt
@@ -1,6 +1,34 @@
 package io.esalenko.findfilmapp
 
+import android.os.Build
 import android.support.multidex.MultiDexApplication
+import com.squareup.leakcanary.LeakCanary
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import java.util.*
 
 
-class App : MultiDexApplication()
+class App : MultiDexApplication(), AnkoLogger {
+
+    private lateinit var locale: Locale
+
+    override fun onCreate() {
+        super.onCreate()
+        // Leak Canary initialization
+        if (LeakCanary.isInAnalyzerProcess(this)) return
+        LeakCanary.install(this)
+    }
+
+    fun getCurrentLocale(): String {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            locale = resources.configuration.locale
+            info { locale.toString() }
+        } else {
+            locale = resources.configuration.locales.get(0)
+            info { locale.toString() }
+        }
+
+        return locale.toString()
+    }
+
+}
