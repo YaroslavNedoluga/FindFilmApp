61871ea4f0b0f53696dd738e7046ad1060af4dc2
Egor Salenko <wrongua@gmail.com>
2017-11-12 21:48:17 +0200

    Delete poster view

--- app/src/main/java/io/esalenko/findfilmapp/fragments/DetailFilmFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/fragments/DetailFilmFragment.kt
@@ -19,6 +19,7 @@ class DetailFilmFragment : BaseFragment(), AnkoLogger {
     @BindView(R.id.tv_fragment_detail_film_overview)
     lateinit var movieOverview: TextView
 
+    @BindView(R.id.tv_fragment_detail_film_release_date)
     lateinit var movieReleaseDate: TextView
 
     @BindView(R.id.tv_fragment_detail_film_title)
@@ -27,21 +28,24 @@ class DetailFilmFragment : BaseFragment(), AnkoLogger {
     @BindView(R.id.iv_fragment_detail_film_backdrop)
     lateinit var movieBackdrop: ImageView
 
+    @BindView(R.id.tv_fragment_detail_film_vote_count)
     lateinit var movieVoteCount: TextView
 
+    @BindView(R.id.tv_fragment_detail_film_average_vote)
     lateinit var movieVoteAvarage: TextView
 
     override fun getLayoutRes(): Int = R.layout.fragment_detail_film
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         showFilmDetails()
     }
 
-    fun showFilmDetails() {
+    private fun showFilmDetails() {
         Handler().post({
-            val film: Film = arguments[MainActivity.FILM] as Film
-            val baseImgUrl = context.resources.getString(R.string.base_url_for_img)
+
+            val film: Film = arguments!![MainActivity.FILM] as Film
+            val baseImgUrl = context?.resources?.getString(R.string.base_url_for_img)
 
             val backDropUrl = baseImgUrl + film.posterPath
             Glide.with(context)
@@ -54,11 +58,11 @@ class DetailFilmFragment : BaseFragment(), AnkoLogger {
             info { "Film overview: " + film.overview }
             movieOverview.text = film.overview
             info { "Film release date : " + film.releaseDate }
-//        movieReleaseDate.text = film.releaseDate
+            movieReleaseDate.text = film.releaseDate?.substring(0, 4)
             info { "Vote count: " + film.voteCount }
-//        movieVoteCount.text = film.voteCount.toString()
+            movieVoteCount.text = getString(R.string.vote_count, film.voteCount)
             info { "Vote average:" + film.voteAverage }
-//        movieVoteAvarage.text = film.voteAverage.toString()
+            movieVoteAvarage.text = getString(R.string.average_vote, film.voteAverage)
         })
     }
 
