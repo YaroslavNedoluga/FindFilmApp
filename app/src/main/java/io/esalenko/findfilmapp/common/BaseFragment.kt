fd01f3e5739845788cc931baad7a5e02e6bee3d1
Egor Salenko <wrongua@gmail.com>
2017-11-12 21:44:53 +0200

    Add extension functions for toasts

--- app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/common/BaseFragment.kt
@@ -6,15 +6,18 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import butterknife.ButterKnife
 import butterknife.Unbinder
+import es.dmoral.toasty.Toasty
+import io.esalenko.findfilmapp.App
 
 abstract class BaseFragment : Fragment() {
 
-    lateinit var unbinder: Unbinder
+    private lateinit var unbinder: Unbinder
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view: View = inflater?.inflate(getLayoutRes(), container, false)!!
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view: View = inflater.inflate(getLayoutRes(), container, false)!!
         unbinder = ButterKnife.bind(this, view)
         return view
     }
@@ -25,5 +28,19 @@ abstract class BaseFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         unbinder.unbind()
+    }
+
+    protected fun getApp(): App = activity?.application as App
+
+    protected fun Fragment.toastError(message: CharSequence, duration: Int = Toast.LENGTH_LONG) {
+        Toasty.error(context!!, message, duration, true).show()
+    }
+
+    protected fun Fragment.toastSuccess(message: CharSequence, duration: Int = Toast.LENGTH_LONG) {
+        Toasty.success(context!!, message, duration, true).show()
+    }
+
+    protected fun Fragment.toastWarning(message: CharSequence, duration: Int = Toast.LENGTH_LONG) {
+        Toasty.warning(context!!, message, duration, true).show()
     }
 }
