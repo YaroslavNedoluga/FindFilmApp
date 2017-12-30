1c874470977c9ee9d3cf6988957d21f4b18e1554
Egor Salenko <wrongua@gmail.com>
2017-12-31 00:07:16 +0200

    Update popular fragment to Material Design principles

--- app/src/main/java/io/esalenko/findfilmapp/fragments/PopularFilmsFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/fragments/PopularFilmsFragment.kt
@@ -4,35 +4,37 @@ package io.esalenko.findfilmapp.fragments
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
 import butterknife.BindView
 import io.esalenko.findfilmapp.R
 import io.esalenko.findfilmapp.adapters.PopularFilmAdapter
+import io.esalenko.findfilmapp.adapters.viewholder.FilmViewHolder
 import io.esalenko.findfilmapp.common.BaseFragment
-import io.esalenko.findfilmapp.model.Film
-import io.esalenko.findfilmapp.viewmodel.PopularFilmsViewModel
+import io.esalenko.findfilmapp.viewmodel.FilmsViewModel
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 
 
 class PopularFilmsFragment : BaseFragment()
-        , PopularFilmAdapter.PopularFilmOnCLickListener
+        , FilmViewHolder.FilmViewHolderCommander
         , AnkoLogger {
 
+    companion object {
+        const val LAST_PAGE: String = "last_page"
+    }
+
     private lateinit var commander: PopularFilmCommander
+    private lateinit var adapter: PopularFilmAdapter
+    private lateinit var viewModel: FilmsViewModel
+    private var page: Int = 1
 
     @BindView(R.id.rv_popular_films_list)
     lateinit var recyclerView: RecyclerView
-
-    private var page: Int = 1
-
-    private lateinit var adapter: PopularFilmAdapter
-
-    private lateinit var viewModel: PopularFilmsViewModel
-
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -43,22 +45,27 @@ class PopularFilmsFragment : BaseFragment()
         }
     }
 
-    override fun getLayoutRes(): Int = R.layout.fragment_popular_films
+    override val layoutRes: Int
+        get() = R.layout.fragment_popular_films
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         info("onViewCreated()")
 
         adapter = PopularFilmAdapter(this)
-        recyclerView.layoutManager = LinearLayoutManager(context)
+        if (activity?.resources?.configuration?.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            recyclerView.layoutManager = LinearLayoutManager(context)
+        } else if (activity?.resources?.configuration?.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        }
         recyclerView.setHasFixedSize(true)
         recyclerView.adapter = this.adapter
-        viewModel = ViewModelProviders.of(this).get(PopularFilmsViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(FilmsViewModel::class.java)
         subscribe()
     }
 
     private fun subscribe() {
-        viewModel.loadPopularFilms(page, getApp().getCurrentLocale())
+        viewModel.loadPopularFilms(page, app.getCurrentLocale())
                 .observe(this, Observer { films ->
                     adapter.setList(films)
                     adapter.notifyDataSetChanged()
@@ -66,12 +73,16 @@ class PopularFilmsFragment : BaseFragment()
                 })
     }
 
-    override fun onPopularFilmClicked(film: Film) {
-        commander.showFilmDetails(film)
+    override fun onFilmViewHolderMore(id: Int) {
+        commander.showFilmDetails(id)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(LAST_PAGE, page)
+    }
 
     interface PopularFilmCommander {
-        fun showFilmDetails(film: Film)
+        fun showFilmDetails(id: Int)
     }
 }
