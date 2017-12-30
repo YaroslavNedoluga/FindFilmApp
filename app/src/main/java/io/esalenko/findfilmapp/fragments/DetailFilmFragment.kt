9f34742d5889f6ab8ab0586bdc5b7d0c3951520c
Egor Salenko <wrongua@gmail.com>
2017-12-31 00:05:55 +0200

    Update detail film fragment to Material Design principles

--- app/src/main/java/io/esalenko/findfilmapp/fragments/DetailFilmFragment.kt
+++ app/src/main/java/io/esalenko/findfilmapp/fragments/DetailFilmFragment.kt
@@ -1,20 +1,31 @@
 package io.esalenko.findfilmapp.fragments
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 import butterknife.BindView
 import com.bumptech.glide.Glide
-import io.esalenko.findfilmapp.MainActivity
 import io.esalenko.findfilmapp.R
+import io.esalenko.findfilmapp.activity.MainActivity
 import io.esalenko.findfilmapp.common.BaseFragment
-import io.esalenko.findfilmapp.model.Film
+import io.esalenko.findfilmapp.model.DetailedFilm
+import io.esalenko.findfilmapp.viewmodel.FilmsViewModel
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
 
 class DetailFilmFragment : BaseFragment(), AnkoLogger {
+
+    companion object {
+        fun newInstance(bundle: Bundle? = null): DetailFilmFragment {
+            val detailFragment = DetailFilmFragment()
+            if (bundle != null) {
+                detailFragment.arguments = bundle
+            }
+            return detailFragment
+        }
+    }
 
     @BindView(R.id.tv_fragment_detail_film_overview)
     lateinit var movieOverview: TextView
@@ -25,45 +36,60 @@ class DetailFilmFragment : BaseFragment(), AnkoLogger {
     @BindView(R.id.tv_fragment_detail_film_title)
     lateinit var movieTitle: TextView
 
-    @BindView(R.id.iv_fragment_detail_film_backdrop)
-    lateinit var movieBackdrop: ImageView
-
-    @BindView(R.id.tv_fragment_detail_film_vote_count)
-    lateinit var movieVoteCount: TextView
+    @BindView(R.id.iv_fragment_detail_film_poster)
+    lateinit var moviePoster: ImageView
 
     @BindView(R.id.tv_fragment_detail_film_average_vote)
     lateinit var movieVoteAvarage: TextView
 
-    override fun getLayoutRes(): Int = R.layout.fragment_detail_film
+    @BindView(R.id.tv_fragment_detail_film_tagline)
+    lateinit var movieTagline: TextView
+
+    @BindView(R.id.tv_fragment_detail_film_genres)
+    lateinit var movieGenres: TextView
+
+    private lateinit var filmViewModel: FilmsViewModel
+
+    override val layoutRes: Int
+        get() = R.layout.fragment_detail_film
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        showFilmDetails()
+        filmViewModel = ViewModelProviders.of(this).get(FilmsViewModel::class.java)
+        filmDetailSubscribe()
     }
 
-    private fun showFilmDetails() {
-        Handler().post({
+    private fun filmDetailSubscribe() {
+        val detailFilmObserver = Observer<DetailedFilm> { film: DetailedFilm? ->
 
-            val film: Film = arguments!![MainActivity.FILM] as Film
-            val baseImgUrl = context?.resources?.getString(R.string.base_url_for_img)
+            if (film != null) {
+                with(film) {
+                    val baseImgUrl = context?.resources?.getString(R.string.base_url_for_img)
 
-            val backDropUrl = baseImgUrl + film.posterPath
-            Glide.with(context)
-                    .load(backDropUrl)
-                    .into(movieBackdrop)
+                    val backDropUrl = baseImgUrl + posterPath
 
-            info { "Film title: " + film.title }
-            val releaseDate = film.releaseDate?.substring(0, 4)
-            movieTitle.text = film.title?.toUpperCase() + " ($releaseDate)"
-            info { "Film overview: " + film.overview }
-            movieOverview.text = film.overview
-            info { "Film release date : " + film.releaseDate }
-            movieReleaseDate.text = film.releaseDate?.substring(0, 4)
-            info { "Vote count: " + film.voteCount }
-            movieVoteCount.text = getString(R.string.vote_count, film.voteCount)
-            info { "Vote average:" + film.voteAverage }
-            movieVoteAvarage.text = getString(R.string.average_vote, film.voteAverage)
-        })
+                    Glide.with(context)
+                            .load(backDropUrl)
+                            .into(moviePoster)
+
+                    movieTitle.text = title?.toUpperCase()
+                    movieOverview.text = overview
+                    movieTagline.text = tagline
+                    var genresConcat: String = ""
+                    for (genre in genres!!) {
+                        genresConcat += " | ${genre.name} | "
+                    }
+                    movieGenres.text = genresConcat
+                    movieReleaseDate.text = releaseDate?.substring(0, 4)
+                    movieVoteAvarage.text = voteAverage.toString()
+
+                }
+            }
+        }
+        val filmId: Int = arguments!!.getInt(MainActivity.FILM_ID)
+        filmViewModel
+                .getFilmByID(filmId, app.getCurrentLocale())
+                .observe(this, detailFilmObserver)
     }
 
 }
