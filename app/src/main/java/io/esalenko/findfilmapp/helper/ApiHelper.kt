9db9e6d53869bdcf6ed20f7fa52ec70157ad0b3b
Egor Salenko <wrongua@gmail.com>
2017-12-31 00:02:42 +0200

    Refactoring code

--- app/src/main/java/io/esalenko/findfilmapp/helper/ApiHelper.kt
+++ app/src/main/java/io/esalenko/findfilmapp/helper/ApiHelper.kt
@@ -1,12 +1,9 @@
 package io.esalenko.findfilmapp.helper
 
-class ApiHelper {
+object ApiHelper {
 
-    private val apiKey = "e155ee391fe67e4bced2832115371e0c"
+    const val apiKey = "e155ee391fe67e4bced2832115371e0c"
 
-    val region = "ISO 3166-1"
-
-    fun getApiKey() = apiKey
-
+    const val region = "ISO 3166-1"
 
 }
