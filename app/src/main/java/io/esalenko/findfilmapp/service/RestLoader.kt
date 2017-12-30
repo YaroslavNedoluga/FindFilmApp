631534dde043c33b4d5154a3ac74e10a3eb9a9c5
Egor Salenko <wrongua@gmail.com>
2017-12-31 00:12:54 +0200

    Add REST getFilmByID request method

--- app/src/main/java/io/esalenko/findfilmapp/service/RestLoader.kt
+++ app/src/main/java/io/esalenko/findfilmapp/service/RestLoader.kt
@@ -2,6 +2,7 @@ package io.esalenko.findfilmapp.service
 
 import io.esalenko.findfilmapp.helper.ApiHelper
 import io.esalenko.findfilmapp.helper.RetrofitHelper
+import io.esalenko.findfilmapp.model.DetailedFilm
 import io.esalenko.findfilmapp.model.Film
 import io.reactivex.Observable
 
@@ -9,15 +10,22 @@ import io.reactivex.Observable
 class RestLoader {
 
     private val restService: RestService = RetrofitHelper().getRetrofitInstance()!!
-    private val apiHelper: ApiHelper = ApiHelper()
 
     fun getPopularFilms(page: Int, locale: String): Observable<List<Film>> {
         return restService
                 .getPopularFilms(
-                        apiHelper.getApiKey(),
+                        ApiHelper.apiKey,
                         locale,
                         page,
-                        apiHelper.region)
+                        locale)
                 .map { popularFilms -> popularFilms.results!! }
+    }
+
+    fun getFilmByID(id: Int, locale: String): Observable<DetailedFilm> {
+        return restService
+                .getFilmByID(
+                        id,
+                        ApiHelper.apiKey,
+                        locale)
     }
 }
--- app/src/main/java/io/esalenko/findfilmapp/service/RestService.kt
+++ app/src/main/java/io/esalenko/findfilmapp/service/RestService.kt
@@ -1,8 +1,10 @@
 package io.esalenko.findfilmapp.service
 
+import io.esalenko.findfilmapp.model.DetailedFilm
 import io.esalenko.findfilmapp.model.PopularFilm
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -10,8 +12,12 @@ interface RestService {
 
     @GET("movie/popular")
     fun getPopularFilms(@Query("api_key") key: String,
-                        @Query("language") language: String,
+                        @Query("language") locale: String,
                         @Query("page") page: Int,
                         @Query("region") region: String): Observable<PopularFilm>
 
+    @GET("movie/{movie_id}")
+    fun getFilmByID(@Path("movie_id") id: Int,
+                    @Query("api_key") api_key: String,
+                    @Query("language") locale: String): Observable<DetailedFilm>
 }
