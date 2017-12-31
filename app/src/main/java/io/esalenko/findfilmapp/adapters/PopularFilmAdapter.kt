6ddfd01373684bc799aa3ec7c632a23f66f48ff3
Egor Salenko <wrongua@gmail.com>
2017-12-31 15:42:55 +0200

    Update popular films adapter diffutil while update

--- app/src/main/java/io/esalenko/findfilmapp/adapters/PopularFilmAdapter.kt
+++ app/src/main/java/io/esalenko/findfilmapp/adapters/PopularFilmAdapter.kt
@@ -1,5 +1,6 @@
 package io.esalenko.findfilmapp.adapters
 
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -7,17 +8,20 @@ import android.view.ViewGroup
 import io.esalenko.findfilmapp.R
 import io.esalenko.findfilmapp.adapters.viewholder.FilmViewHolder
 import io.esalenko.findfilmapp.model.Film
+import io.esalenko.findfilmapp.utils.FilmDiffUtilCallback
 import java.util.*
 
 
 class PopularFilmAdapter(private val callback: FilmViewHolder.FilmViewHolderCommander) : RecyclerView.Adapter<FilmViewHolder>() {
 
-    private val list: ArrayList<Film>? = ArrayList()
+    private val list: ArrayList<Film>? = ArrayList<Film>()
 
     fun setList(list: List<Film>?) {
-        if (list != null) {
-            this.list?.addAll(list)
+        if (this.list?.size == 0) {
+            this.list.addAll(list!!)
         }
+        val diffResult: DiffUtil.DiffResult = DiffUtil.calculateDiff(FilmDiffUtilCallback(this.list!!, list!!))
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): FilmViewHolder {
