cefddf502afc09ef0db3b6fb5a1e4a6d2ed41c28
Egor Salenko <wrongua@gmail.com>
2018-01-06 00:55:10 +0200

    Add base bottom nav view (debug ver)

--- app/src/main/java/io/esalenko/findfilmapp/activity/MainActivity.kt
+++ app/src/main/java/io/esalenko/findfilmapp/activity/MainActivity.kt
@@ -1,11 +1,14 @@
 package io.esalenko.findfilmapp.activity
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
+import butterknife.BindView
 import io.esalenko.findfilmapp.R
-import io.esalenko.findfilmapp.common.BaseActivity
+import io.esalenko.findfilmapp.activity.common.BaseActivity
 import io.esalenko.findfilmapp.fragments.DetailFilmFragment
 import io.esalenko.findfilmapp.fragments.PopularFilmsFragment
+import io.esalenko.findfilmapp.fragments.RandomFilmFragment
 
 class MainActivity : BaseActivity(),
         PopularFilmsFragment.PopularFilmCommander {
@@ -14,12 +17,28 @@ class MainActivity : BaseActivity(),
         val FILM_ID: String = "film"
     }
 
+    @BindView(R.id.bnv_activity_main_nav)
+    lateinit var bnv: BottomNavigationView
+
     override fun getLayoutRes(): Int = R.layout.activity_main
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
             replaceFragment(PopularFilmsFragment())
+        }
+        bnv.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_pop -> {
+                    replaceFragment(PopularFilmsFragment())
+                    true
+                }
+                R.id.nav_random -> {
+                    replaceFragment(RandomFilmFragment())
+                    true
+                }
+                else -> false
+            }
         }
     }
 
